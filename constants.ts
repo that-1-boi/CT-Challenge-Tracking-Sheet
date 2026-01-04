be1138d2836e4ca5b80df64fa108f1f0874a36f3
that-1boi <120428653+that-1-boi@users.noreply.github.com>
2026-01-03 23:12:39 -0500

    updated constants

--- constants.ts
+++ constants.ts
@@ -1,4 +1,4 @@
-import { ClassSession } from './types';
+import { ClassSession, Theme } from './types';
 
 export const DEFAULT_CLASSES: ClassSession[] = [
   { id: 'sat-am1', name: 'Sat AM1', students: [] },
@@ -7,6 +7,29 @@ export const DEFAULT_CLASSES: ClassSession[] = [
   { id: 'sun-pm1', name: 'Sun PM1', students: [] },
   { id: 'sun-pm2', name: 'Sun PM2', students: [] },
   { id: 'unassigned', name: 'Unassigned', students: [] },
+];
+
+export const DEFAULT_THEMES: Theme[] = [
+  {
+    name: 'Robotics Workshop',
+    challenges: ['Build Base', 'Wire Sensors', 'Simple Path', 'Avoid Walls', 'Remote Control'],
+    classes: JSON.parse(JSON.stringify(DEFAULT_CLASSES))
+  },
+  {
+    name: 'Advanced Sensors',
+    challenges: ['Line Follow', 'Color Match', 'Distance Stop', 'Maze Solver', 'Speed Test'],
+    classes: JSON.parse(JSON.stringify(DEFAULT_CLASSES))
+  },
+  {
+    name: 'Coding Fundamentals',
+    challenges: ['Loops 101', 'If Statements', 'Variables', 'Function Fun', 'Debug Master'],
+    classes: JSON.parse(JSON.stringify(DEFAULT_CLASSES))
+  },
+  {
+    name: 'Game Design',
+    challenges: ['Character Move', 'Collision', 'Score System', 'Level 2', 'Boss Fight'],
+    classes: JSON.parse(JSON.stringify(DEFAULT_CLASSES))
+  }
 ];
 
 export const STORAGE_KEY = 'classroom_tracker_state_v2';
